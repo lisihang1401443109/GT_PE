5638712af7c7ba35493f9c12b6b780558c483059
Sihang Li <lisihang1401443109@gmail.com>
2026-04-21 05:19:02 -0400

    Fix PyTorch/PyG version mismatch and pin lightning

--- Benchmarking-PEs/setup_environment.sh
+++ Benchmarking-PEs/setup_environment.sh
@@ -16,15 +16,16 @@ conda create -p "$ENV_PATH" python=3.9 -y
 conda activate "$ENV_PATH"
 
 echo "Installing PyTorch 2.3.0..."
-conda install pytorch==2.3.0 torchvision==0.18.0 torchaudio==2.3.0 pytorch-cuda=12.1 -c pytorch -c nvidia -y
+# Install torch via pip to stay consistent with extensions
+pip install torch==2.3.0 torchvision==0.18.0 torchaudio==2.3.0 --index-url https://download.pytorch.org/whl/cu121
 
 echo "Installing PyG and related libraries..."
-pip install pyg_lib torch_scatter torch_sparse torch_cluster torch_spline_conv -f https://data.pyg.org/whl/torch-2.3.0+cu121.html
-conda install pyg -c pyg -y
+pip install torch-scatter torch-sparse torch-cluster torch-spline-conv -f https://data.pyg.org/whl/torch-2.3.0+cu121.html
+pip install torch-geometric
 
 echo "Installing additional dependencies..."
 conda install openbabel fsspec rdkit -c conda-forge -y
-pip install yacs torchmetrics performer-pytorch ogb tensorboardX wandb torch_ppr attrdict opt_einsum graphgym loguru pytorch_lightning
+pip install yacs torchmetrics performer-pytorch ogb tensorboardX wandb torch_ppr attrdict opt_einsum graphgym loguru pytorch_lightning==2.2.0
 pip install setuptools==59.5.0
 
 echo "Cleaning up conda cache..."
